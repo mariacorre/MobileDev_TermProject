3b42a5cab9caa532146ea7b0b847c1506a218dbf
mariacorre <theresacorre@gmail.com>
2022-04-15 22:13:45 -0400

    added SelectQuizActivity

---
+++ app/src/main/java/com/myassignment/SelectQuizActivity.java
@@ -0,0 +1,13 @@
+package com.myassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SelectQuizActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_options);
+    }
+}
